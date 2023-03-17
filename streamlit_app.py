83b3c30bb35b5e6853822516f42944bdd86d491d
engdell <leif.engdell@gmail.com>
2023-03-17 15:41:07 +0100

    clean up the json from the api

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Normalise the response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# show the normalised data
+streamlit.dataframe(fruityvice_normalized)
