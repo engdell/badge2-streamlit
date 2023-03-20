2e2010cc8459fb554c982ee99eac97de3b0f82e6
engdell <leif.engdell@gmail.com>
2023-03-20 10:20:23 +0100

    push to db but fail

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,7 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+#this will not work for now
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST
+values ('from streamlit')")
