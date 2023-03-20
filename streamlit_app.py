cc4a5116f671c7e1bca56e86c2e5fc7842d41341
engdell <leif.engdell@gmail.com>
2023-03-20 15:25:02 +0100

    create function for fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,18 +30,24 @@ streamlit.dataframe(fruits_to_show)
 # google spreadsheet guid: 1Bp_v3ngu0og3euh0oTTP1KKtkGzwRVf4zqCECvsDJjo
 
 #New section to display api response
+#create a function
+def get_fruityvice_data(this_fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
             streamlit.error("Please select a fruit to get information")
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        back_from function = get_fruityvice_data(fruit_choice)
         #streamlit.text(fruityvice_response.json())
         # Normalise the response
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        
         # show the normalised data
-        streamlit.dataframe(fruityvice_normalized)
+        streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlite.error()
 
