7471dc4248d7efefa67989c2ab3726f845c0c475
engdell <leif.engdell@gmail.com>
2023-03-20 10:15:50 +0100

    wrong fruit var

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,4 @@ streamlit.header("The fruitload list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
-streamlit.write('Thanks for adding ', fruit_choice)
+streamlit.write('Thanks for adding ', add_my_fruit)
