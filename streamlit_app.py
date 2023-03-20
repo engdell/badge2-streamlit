84f54f1da83ccf5a5888398ce7a07a50ed324897
engdell <leif.engdell@gmail.com>
2023-03-20 18:43:07 +0100

    last add button and func refaktor

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,11 +68,20 @@ if streamlit.button('Get Fruit load list'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 #dont run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 #this will not work for now
 #my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx_cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return "Thanks for adding " + new_fruit
+add_my_fruit = streamlit.text_input("What fruit would you like to add?")
+if streamlit.button('Add a Fruit to the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
