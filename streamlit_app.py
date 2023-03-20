a55771b0fd02a4f253044eadfa38bef96c0d4edc
engdell <leif.engdell@gmail.com>
2023-03-20 11:01:59 +0100

    fix indent2

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,16 +35,13 @@ try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
     if not fruit_choice:
             streamlit.error("Please select a fruit to get information")
-            else:
-
-#streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# Normalise the response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# show the normalised data
-streamlit.dataframe(fruityvice_normalized)
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        #streamlit.text(fruityvice_response.json())
+        # Normalise the response
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        # show the normalised data
+        streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
     streamlite.error()
 
